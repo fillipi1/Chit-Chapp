3c60518279842708cd06a1ecef6be884497e7d1e
Fillipi <fillipi@Fillipis-MBP.impekable.com>
2018-09-04 17:29:39 -0700

    setting up store-actions/reducers

--- src/views/Header/headerActions.js
+++ src/views/Header/headerActions.js
@@ -2,28 +2,11 @@ import store from 'store';
 
 const { dispatch } = store;
 
-export function setSubGreeting({ subGreeting }) {
-  // Destructure and restructure the payload so it is
-  // easy to see what is being passed as the payload
-
-  // Any logic should be preformed in the action, prior to dispatching
-  const propCaseGreeting = stringToProperCase(subGreeting);
-
-  // Wrapping the returned object in the dispatch() call is unconventional,
-  // but the conventional method of mapping dispatch to props and importing
-  // the action is unnecessarily redundant
+export function selectUser({user}) {
+  
   return dispatch({
-    type: 'SET_SUBGREETING',
-    payload: { subGreeting: propCaseGreeting }
+    type: 'SELECT_USER',
+    payload: { user }
   });
 }
 
-function stringToProperCase(inputString) {
-  return inputString.split(' ').map(word => {
-    const chars = word.split('');
-
-    chars[0] = chars[0].toUpperCase();
-
-    return chars.join('');
-  }).join(' ');
-}

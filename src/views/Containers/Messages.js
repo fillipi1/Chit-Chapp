f06b54a918e76a3f225333ea59892ac5de869b71
Fillipi <fillipi@Fillipis-MBP.impekable.com>
2018-09-10 17:54:21 -0700

    message bubbles fixed

--- src/views/Containers/Messages.js
+++ src/views/Containers/Messages.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Paper } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {compose} from 'redux';
@@ -13,40 +12,40 @@ render () {
   if (!this.props.user){
     return  (
       <Paper style={style.paper}>
-    <div align='center'>
-    <Button variant = 'contained' style={style.button} color= 'primary'>
-    <Typography style={style.welcome}>
-    WELCOME TO CHIT CHAPP,
-    click on a user to open messages
-    </Typography>
-    </Button>
-    </div>
-    </Paper>
+        <div align='center'>
+          <Button variant = 'contained' style={style.button} color= 'primary'>
+            <Typography style={style.welcome}>
+              WELCOME TO CHIT CHAPP,
+              click on a user to open messages
+            </Typography>
+          </Button>
+        </div>
+       </Paper>
     )
   }
   return (
     <Paper style={style.paper} >
-
-      <div style={style.block} >
-      <Typography variant= 'body1' style={style.message1}>{this.props.user.chat1}</Typography>
-      </div>
-      <Typography variant= 'caption' style={style.caption2}>Yesterday 8:30 PM</Typography>
-
-      <div style={style.block2} >
-      <Typography variant= 'body1' style={style.message2}>{this.props.user.chat2}</Typography>
-      <Typography variant= 'caption' style={style.caption3}>delivered</Typography>
-      </div>
-
-      <div style={style.block} >
-      <Typography variant= 'body1' style={style.message1}>{this.props.user.chat3}</Typography>
-      </div>
-
-      <Typography variant= 'caption' style={style.caption2}>Today 11:05 AM</Typography>
-
-      <div style={style.block2} >
-      <Typography variant= 'body1' style={style.message2}>{this.props.user.chat4}</Typography>
-      <Typography variant= 'body1' style={style.message2}>{this.props.user.chat5}</Typography>
-      <Typography variant= 'caption' style={style.caption3}>delivered</Typography>
+      <div style ={style.messageListStyle}>
+        <div style={style.block} >
+          <Typography variant= 'body1' style={style.message1}>{this.props.user.chat1}</Typography>
+        </div>
+          <Typography variant= 'caption' style={style.caption2}>Yesterday 8:30 PM</Typography>
+        <div style={style.block2} >
+          <Typography variant= 'body1' style={style.message2}>{this.props.user.chat2}</Typography>
+        </div>
+        <div style={style.block} >
+          <Typography variant= 'body1' style={style.message1}>{this.props.user.chat3}</Typography>
+        </div>
+        <Typography variant= 'caption' style={style.caption2}>Today 11:05 AM</Typography>
+        <div style={style.block2} >
+          <Typography variant= 'body1' style={style.message2}>{this.props.user.chat4}</Typography>
+        </div>
+        <div style={style.block2} >
+          <Typography variant= 'body1' style={style.message2}>{this.props.user.chat5}</Typography>
+        </div>
+        <div style={style.block2}>
+        <Typography variant= 'caption' style={style.caption3}>delivered</Typography>
+        </div>
       </div>
     </Paper>
 
@@ -60,54 +59,58 @@ const style = {
   paper: {
     padding:0, height:'100vh'
   },
+  messageListStyle: {
+    position: 'relative',
+    height: '100vh',
+    overflowY: 'scroll',
+    paddingBottom: '150px',
+  },
   welcome:{
     padding: 30,
     borderRadius: '20px',
     maxWidth: 170,
     color: '#ffffff',
-  
-    },
+ },
   message1:{
-  background: '#4b49521f',
-  padding: 10,
-  borderRadius: '20px',
-  fontSize: '13px',
-  marginBottom: '1.1em',
-  marginLeft: '1 em',
-  lineHeight: '1.5em',
-  fontFamily: 'Roboto, sans-serif',
-
+    background: '#4b49521f',
+    padding: 10,
+    borderRadius: '20px',
+    fontSize: '0.8em',
+    marginBottom: '1.1em',
+    marginLeft: '1 em',
+    lineHeight: '1.5em',
+    fontFamily: 'Roboto, sans-serif',
   },
 
   message2:{
     background: '#0024d4e3',
-  padding: 10,
-  borderRadius: '20px',
-  fontSize: '0.8em',
-  marginBottom: '1.1em',
-  marginLeft: 60,
-  lineHeight: '1.5em',
-  fontFamily: 'Roboto, sans-serif',
-  color: '#ffffff',
-
+    padding: 10,
+    borderRadius: '20px',
+    fontSize: '0.8em',
+    marginBottom: '1.1em',
+    marginLeft: '1 em',
+    lineHeight: '1.5em',
+    fontFamily: 'Roboto, sans-serif',
+    color: '#ffffff',
+    maxWidth: '50%'
   },
 
   block: {
     padding:20,
-    maxWidth: 150,
+    maxWidth: '50%',
+    display: 'flex'
   },
 
   block2: {
     padding: 10,
-    marginLeft: '60%',
-    
-
+    display: 'flex',
+    flexDirection: 'row-reverse',
   },
   caption1: {
     marginLeft: 30,
   },
   caption2: {
-    marginLeft: 230,
+    marginLeft: '50%',
   },
   caption3: {
     marginLeft: 90,

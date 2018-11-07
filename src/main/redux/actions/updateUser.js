cfacde4cc26e8f67fc14c74eb8cade29d9507c8e
Fillipi <fillipi@eduroam-169-233-139-84.ucsc.edu>
2018-11-07 13:44:55 -0800

    agent key = agent id

--- src/main/redux/actions/updateUser.js
+++ src/main/redux/actions/updateUser.js
@@ -7,16 +7,22 @@ export function updateUsers(user){
 
 export function loginUser(){
   console.log('called')
-  firebase.auth().createUserWithEmailAndPassword('FillipiRochat@impekable.com', 'testpassword').then((response) => {
+  firebase.auth().createUserWithEmailAndPassword('Fillip111@gmail.com', 'testpassword').then((response) => {
     const MY_ID = response.user.uid;
     const MY_EMAIL = response.user.email;
-    var userRef = firebase.database().ref('agent');
-    userRef.push({
+    var userRef = firebase.database().ref('agent').set({[response.user.uid]: {
       id: MY_ID,
       name: 'Fillipi',
       email: MY_EMAIL,
       phone: "+15103437234"
-    }).then(() => {
+    }})
+    // userRef.push({
+    //   id: MY_ID,
+    //   name: 'Fillipi',
+    //   email: MY_EMAIL,
+    //   phone: "+15103437234"
+    // })
+    .then(() => {
       return {
         type: 'LOG_IN',
       payload: user
@@ -35,8 +41,27 @@ export function addUser(email, password, name) {
     }).then(function(response) {
       console.log(response.key)
       const customerId = response.key
-      const a = firebase.database().ref(`customers/${customerId}`);
+      const chatRef = firebase.database().ref('chat');
+      chatRef.push({
+        members: {customer: customerId, agent: "dBMGQE4T23a6cbTGLIS4gD2ljr13"}
+      }).then(function(response) {
+       const messageId = response.key
+       const messageRef = firebase.database().ref('messages(trial)');
+        messageRef.push({
+          Id: messageId
+        })
+        console.log('char id' + response.key)
+      })
+      //create new chat then save chatUID in both agent and customer
     });
   }
 }
   
+ //const a = firebase.database().ref(`agents/${customerId}/`);
+ //next steps: 1) make sure agents key is = agentId 2) 
+//  firebase.database().ref("pathName").set({[variable] : 'MoreStuff'});
+// Option 2
+// var id="30";
+// var updatedObj = {};
+// updatedObj[id] = true;
+// ref.child("someChild").update(updatedObj);
